5be5a23b41443b1d7f00cf32ec627b1d783ebd54
Nitin Nayyar <nayyarnitin09@gmail.com>
2017-03-18 22:19:26 -0400

    clean files

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,35 @@ var gulp=require('gulp');
 var sass=require('gulp-sass');
 var browserSync=require('browser-sync');
 var reload = browserSync.reload;
+var clean=require('gulp-clean');
 gulp.task('nitin',function(){
-  return gulp.src('src/scss/app.scss').pipe(gulp.dest('app/css/app.css'));
+  return gulp.src('src/scss/app.scss').pipe(gulp.dest('app/css'));
   });
 var APPPATH={
-  root:'app/'
+  root:'app/',
+  roothtml:'src/*.html'
 }
 
-gulp.task('serve',function()
+gulp.task('clean',function()
 {
-  browserSync.init([APPPATH.root + '/*.html'],
+  gulp.src(APPPATH.root+'/*.html',{read:false,force:true})
+  .pipe(clean())
+});
+
+gulp.task('copy',['clean'],function()
+{
+  gulp.src(APPPATH.roothtml)
+  .pipe(gulp.dest(APPPATH.root))
+});
+
+gulp.task('watch',['copy'],function()
+{
+  gulp.watch([APPPATH.roothtml],['copy'])
+});
+
+gulp.task('serve',['watch'],function()
+{
+  browserSync.init([APPPATH.root + 'css/'+'/*.css',APPPATH.root + '/*.html'],
   {
     server:{
       baseDir:APPPATH.root
